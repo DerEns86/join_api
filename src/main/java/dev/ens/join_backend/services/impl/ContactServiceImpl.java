e1277cb38aae1877d8928efdc39dea213347a88f
DerEns86 <v.ens86@yahoo.de>
2025-02-18 15:18:22 +0100

    delete unused imports

--- src/main/java/dev/ens/join_backend/services/impl/ContactServiceImpl.java
+++ src/main/java/dev/ens/join_backend/services/impl/ContactServiceImpl.java
@@ -1,18 +1,13 @@
 package dev.ens.join_backend.services.impl;
 
-import dev.ens.join_backend.model.Category;
 import dev.ens.join_backend.model.Contact;
 import dev.ens.join_backend.model.User;
-import dev.ens.join_backend.model.enums.UpdateMessage;
-import dev.ens.join_backend.repository.CategoryRepository;
 import dev.ens.join_backend.repository.ContactRepository;
 import dev.ens.join_backend.repository.UserRepository;
-import dev.ens.join_backend.services.CategoryService;
 import dev.ens.join_backend.services.ContactService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
 
